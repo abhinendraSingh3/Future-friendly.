a0ea8a54cbf90acac0a54930fdc45319e1b6dd52
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2022-10-18 10:18:39 +0530

    Create starPatter.py

---
+++ starPatter.py
@@ -0,0 +1,8 @@
+rows=5 #here we are showing how many lines do we want
+#i loop is to iterage through rows
+for i in range(0,rows):
+  #j loop is to set the increasing pattern of the stars
+  for j in range(0,i+1):
+    print('*',end='')
+    #this statement is to print new line after fulfilling the requirements
+  print('')  
