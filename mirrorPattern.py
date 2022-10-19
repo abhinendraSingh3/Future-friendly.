9b4f0aecc16ff9817516c201f435d6ebdf275a48
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2022-10-19 20:43:20 +0530

    Update mirrorPattern.py

--- mirrorPattern.py
+++ mirrorPattern.py
@@ -1,3 +1,13 @@
 n=int(input("Enter the number of stars to be printed"))
 for i in range(n):
   print(""*(n-i-1)+"*"*(i+1))
+  
+  #OR
+  
+n = int(input().strip())
+for i in range(n):
+    for j in range(n-i-1):
+         print(" ",end="")
+    for j in range(i+1):
+          print("#",end="")
+    print()
