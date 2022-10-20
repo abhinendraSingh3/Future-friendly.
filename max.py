5a0ab06c53ef13fc079a337df01559625d18d811
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2022-10-20 16:11:34 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,17 @@
+def birthdayCakeCandles(candles):
+    
+    return candles.count(max(candles))
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
