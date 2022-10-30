9802c2833c49d93bfb8d5012f1a962c9b6f3f158
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-10-30 13:56:28 +0530

    some

--- sample.py
+++ sample.py
@@ -4,10 +4,17 @@
 from binarytree import Node
 root=Node(3)
 root.left=Node(5)
-root.Right=Node(8)
+root.right=Node(8)
 
 #binary tree printing
 print('Binary tree:',root)
 
 #getting list of nodes
-print
+print('List of nodes',list(root))
+
+#inorder traversal
+print('inorders',root.inorder)
+
+#checking properties of tree
+print('Size of tree',root.size)
+print('Height of tree',root.height)
