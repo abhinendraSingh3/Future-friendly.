46cafe3b33573e07a2538dc815d72b910d86c835
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-02 19:21:07 +0530

    initiated the commit

--- practice.js
+++ practice.js
@@ -1,10 +1,19 @@
-for (var i = 0; i < 100; i++)
-{
-    // check that the number is even
-    if (i % 2 == 0)
-    {
-         continue;
-    }
-    // if we got here, then i is odd.
-    console.log(i + " is an odd number.");
+// for (var i = 0; i < 100; i++)
+// {
+//     // check that the number is even
+//     if (i % 2 == 0)
+//     {
+//          continue;
+//     }
+//     // if we got here, then i is odd.
+//     console.log(i + " is an odd number.");
+// }
+const greet=(name)=>{
+    console.log("hello"+ name)
 }
+greet("Ankit")
+
+const gm=(msg,name)=>{
+    return msg+" "+name;
+}
+console.log(gm("Fuck you all","Naman"))
