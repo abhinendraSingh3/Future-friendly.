e8f1cc674f65529b71ff864932a24463d20d31ba
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-02 19:46:24 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -36,15 +36,3 @@
 // }
 // console.log(extractDetails())
 
-const persons = [
-    {firstname : "Malcom", lastname: "Reynolds"},
-    {firstname : "Kaylee", lastname: "Frye"},
-    {firstname : "Jayne", lastname: "Cobb"}
-  ];
-  
-  persons.map(getFullName);
-  
-  function getFullName(item) {
-    return [item.firstname,item.lastname].join(" ");
-  }
-  getFullName();
