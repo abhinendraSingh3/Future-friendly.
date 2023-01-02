ae50122ebe153269a6f4c323689003e2f76eaa85
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-02 19:46:04 +0530

    js commit

--- practice.js
+++ practice.js
@@ -8,12 +8,43 @@
 //     // if we got here, then i is odd.
 //     console.log(i + " is an odd number.");
 // }
-const greet=(name)=>{
-    console.log("hello"+ name)
-}
-greet("Ankit")
 
-const gm=(msg,name)=>{
-    return msg+" "+name;
-}
-console.log(gm("Fuck you all","Naman"))
+//------ Arrow function--------//
+
+// const greet=(name)=>{
+//     console.log("hello"+ name)
+// }
+// greet("Ankit")
+
+// const gm=(msg,name)=>{
+//     return msg+" "+name;
+// }
+// console.log(gm("Fuck you all","Naman"))
+
+//---------------------------------------------
+// const greet=name=> "hello "+ name;
+// console.log(greet("JoD"));
+
+//------------js map function-----------
+// const details=[
+//     {firstN:"JoD",lastN:"kakashi"},
+//     {firstN:"Abhi",lastN:"Yadav"}
+// ];
+// details.map(extractDetails);
+// function extractDetails(items){
+//     return [items.firstN,items.lastN].join(" ");
+// }
+// console.log(extractDetails())
+
+const persons = [
+    {firstname : "Malcom", lastname: "Reynolds"},
+    {firstname : "Kaylee", lastname: "Frye"},
+    {firstname : "Jayne", lastname: "Cobb"}
+  ];
+  
+  persons.map(getFullName);
+  
+  function getFullName(item) {
+    return [item.firstname,item.lastname].join(" ");
+  }
+  getFullName();
